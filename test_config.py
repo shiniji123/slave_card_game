709a5557e97807733822ef751d9fd13384854dc8
shiniji123 <anucha.sae@student.mahidol.edu>
2025-12-17 17:14:13 +0700

    test

---
+++ test_config.py
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkA2oF2pmpVOTVG3fj3D95mkmzia-foPI",
+  authDomain: "slave-card-game.firebaseapp.com",
+  projectId: "slave-card-game",
+  storageBucket: "slave-card-game.firebasestorage.app",
+  messagingSenderId: "319088373934",
+  appId: "1:319088373934:web:18e4191d7621cb88f3fd3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
